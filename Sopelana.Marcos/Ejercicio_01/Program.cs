af9aec282c21032a00352fe30d1985ebd80951f4
sopelan <sopelan@gmail.com.ar>
2019-08-20 15:06:19 -0300

    Signed-off-by: sopelan <sopelan@gmail.com.ar>

---
+++ Sopelana.Marcos/Ejercicio_01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 01";
+            Console.WriteLine("hola mundo");
+            /*string nombre = "marcos sopelana";
+            int edad = 18;
+            nombre = Console.ReadLine();
+            Console.WriteLine("mi nombre es {0} y tengo {1}",nombre,edad);*/
+            int num;
+            int suma = 0;
+            float promedio;
+            int valorMinimo = int.MaxValue;
+            int valorMaximo = int.MinValue;
+            int i;
+            for(i = 0; i < 5; i++)
+            {
+                num = int.Parse(Console.ReadLine());
+                suma = suma + num;
+                if (num > valorMaximo)
+                    valorMaximo = num;
+                if (num < valorMinimo)
+                    valorMinimo = num;
+            }
+            promedio = suma / 5f;
+            Console.WriteLine("la suma total es {0}",suma);
+            Console.WriteLine("el promedio es {0}", promedio);
+            Console.WriteLine("el valor minimo es {0}",valorMinimo);
+            Console.WriteLine("el valor maximo es {0}", valorMaximo);
+            Console.Read();
+        }
+    }
+}
