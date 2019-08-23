8e8ba21cf318b9ad928a79e74a56038aadaea85f
sopelan <marcossopelana@gmail.com>
2019-08-23 16:52:23 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ Sopelana.Marcos/Ejercicio_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int i;
+            int num;
+            Console.WriteLine("ingrese un numero");
+            num = int.Parse(Console.ReadLine());
+            for (i = 1; i < num + 1 ; i += 1)
+            {
+                if(num % i == 0)
+                    a++;
+            }
+            if (a != 2)
+                Console.WriteLine("no es primo");
+            else
+                Console.WriteLine("es primo");
+            Console.Read();
+        }
+    }
+}
