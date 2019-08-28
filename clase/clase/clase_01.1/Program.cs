27d6b46f13fdf30c08f16c5a2af3eb93c64b7fe3
sopelan <marcossopelana@gmail.com>
2019-08-27 23:01:06 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ clase/clase/clase_01.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*1. Generar un nuevo programaque realicelas siguientes acciones:
+    A.Ingresar 5 números enteros por consola, guardándolos en 5 variables escalares. Luego calcular y mostrar: el valor máximo, el valor mínimo y el promedio.*/
+namespace clase_01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int i;
+            int valorMaximo = 0;
+            int valorMinimo = 0;
+            int suma = 0;
+            float promedio;
+
+            for (i = 0 ; i < 5; i++)
+            {
+                Console.WriteLine("ingrese el numero {0}", i + 1);
+                num = Convert.ToInt32(Console.ReadLine());
+                if(i == 0)
+                {
+                    valorMaximo = num;
+                    valorMinimo = num;
+                    suma = num;
+                }
+                else
+                {
+                    suma += num;
+                    if (valorMaximo < num)
+                        valorMaximo = num;
+                    if (valorMinimo > num)
+                        valorMinimo = num;
+                }
+            }
+            promedio = suma / 5f;
+            Console.WriteLine("el valor maximo es {0}", valorMaximo);
+            Console.WriteLine("el valor minimo es {0}", valorMinimo);
+            Console.WriteLine("el promedio es {0}", promedio);
+            Console.Read();
+        }
+    }
+}
---
+++ clase/clase/clase_01.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* . Siendo la definición de número primo “un número natural mayor que 1 que tiene únicamente dos divisores distintos: él mismo y el 1.”:
+A. Mostrar por pantalla todos los números primos que haya hasta el número que ingrese el usuario por consola*/
+namespace clase_01._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int i;
+            int j;
+            int contador;
+            Console.WriteLine("ingrese un numero");
+            num = Convert.ToInt32(Console.ReadLine());
+            for(i = 1; i <= num; i++)
+            {
+                contador = 0;
+                for (j = 1; j <= i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        contador++;
+                    }  
+                }
+                if (contador == 2)
+                    {
+                        Console.WriteLine(i);
+                    }
+            }
+            Console.Read();
+        }
+    }
+}
