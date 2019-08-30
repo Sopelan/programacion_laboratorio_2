82f6d8f59f7374723acf9b8600fbc8025a7fcad5
sopelan <marcossopelana@gmail.com>
2019-08-30 18:46:24 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ clase/clase/Conversor/Conversor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Desarrollar una clase llamada Conversor.  
+      1. Deberá poseer dos métodos de clase (estáticos):
+      A. string EnteroBinario(int). Convierte un número de entero a binario ascii (un caracter por bit, siendo “111” el equivalente al número 7).
+      B. double BinarioDecimal(string). Convierte un número binario a decimal */
+
+namespace Conversor
+{
+    public class Class
+    {
+        public static string EnteroBinario(int num)
+        {
+            string conversor = "";
+            int j = 1;
+            if(num == 1)
+                return "1";
+
+            if(num == 0)
+                return "0";
+            while(num >= j)
+                j = j * 2; 
+            while(j > 1)
+            {
+                j /= 2; 
+                if (num >= j)
+                {
+                    conversor += "1";
+                    num -= j;
+                }
+                else
+                    conversor += "0";
+            }
+            return conversor;
+        }
+        public static double BinarioDecimal(string num)
+        {
+            return 1;
+        }
+      
+    }
+}
---
+++ clase/clase/clase 02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Conversor;
+/* 2. El en Main generar un menú para que el usuario elija que tipo de conversión desea realizar o si desea salir del programa. 
+      Luego de elegir una opción, le deberá pedir que ingrese el número a convertir y mostrarle el resultado por pantalla; al presionar cualquier tecla retornar al menu. */
+namespace clase_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int option = 1;
+            int num1;
+            string num2;
+            string numConvert1;
+            double numConvert2;
+            while(option != 3)
+            {
+                Console.WriteLine("1 - convertir de binario a entero");
+                Console.WriteLine("2 - convertir de entero a binario");
+                Console.WriteLine("3 - salir");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("ingresar un entero para convertir a binario");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        numConvert1 = Class.EnteroBinario(num1);
+                        Console.WriteLine("el numero en binario es " + numConvert1);
+                        break;
+                    case 2:
+                        Console.WriteLine("ingresar un binario para convertir a entero");
+                        num2 = Console.ReadLine();
+                        numConvert2 = Class.BinarioDecimal(num2);
+                        Console.WriteLine("el numero en entero es " + numConvert2);
+                        break;
+                    case 3:
+                        Console.WriteLine("salir");
+                        break;
+                    default:
+                        Console.WriteLine("numero incorrecto");
+                        break;
+                }
+
+            }
+            Console.Read();
+        }
+    }
+}
