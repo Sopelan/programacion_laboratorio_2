6dbd8303d38a6caaf3eeb9434be8d3dc99d10ab6
sopelan <marcossopelana@gmail.com>
2019-09-03 22:29:28 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ clase/clase/clase_05/Estante.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_05
+{
+    public class Estante
+    {
+        private Producto producto[];
+        private int ubicacionEstantes;
+        private Estante(int capacidad)
+        {
+            
+        }
+        public Estante(int capacidad, int ubicacion):this(capacidad)
+        {
+            this.ubicacionEstantes = ubicacion;
+        }
+        public Producto[] GetProducto()
+        {
+            return this.producto[];
+        }
+        public string MostrarEstante (Estante e)
+        {
+            return e
+        }
+        public static bool operator ==(Estante e , Producto p)
+        {
+
+        }
+        public static bool operator !=(Estante e, Producto p)
+        {
+
+        }
+        public static bool operator +(Estante e , Producto p)
+        {
+
+        }
+        public static Estante operator -(Estante e , Producto p)
+        {
+
+        }
+
+    }
+}
---
+++ clase/clase/clase_05/Producto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_05
+{
+    public class Producto
+    {
+        private string codigoDeBarra;
+        private string marca;
+        private float precio;
+        public Producto(string marca , string codigo , float precio)
+        {
+            this.codigoDeBarra = codigo;
+            this.marca = marca;
+            this.precio = precio;
+        }
+        public string GetMarca()
+        {
+            return this.marca;
+        }
+        public float GetPrecio()
+        {
+            return this.precio;
+        }
+        public static string MostrarProducto(Producto p)
+        {
+            return "la marca es "+ p.marca +" el precio es " + p.precio + " el codigo de barra es " + p.codigoDeBarra;
+        }
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarra;
+        }
+        public static bool operator == (Producto p , string marca)
+        {
+            return (p.marca == marca);
+        }
+        public static bool operator != (Producto p , string marca)
+        {
+            return !(p == marca);
+        }
+        public static bool operator == (Producto p1 , Producto p2)
+        {
+            return ((p1.marca == p2.marca) && (p1.codigoDeBarra == p2.codigoDeBarra));
+        }
+        public static bool operator != (Producto p1 , Producto p2)
+        {
+            return !(p1 == p2); 
+        }
+
+
+    }
+}
