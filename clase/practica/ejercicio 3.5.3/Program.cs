fcb7c6692ccb565c546a5ec52be5dd3893d5fdc9
sopelan <marcossopelana@gmail.com>
2019-10-15 20:52:28 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ clase/practica/ejercicio 3.5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Crear un programa que use el operador condicional para dar a una variable llamada "ambosPares" (booleana) el valor "true" si dos números introducidos por el usuario son pares, o "false" si alguno es impar.  
+namespace ejercicio_3._5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ambosPares;
+            int num1;
+            int num2;
+            Console.WriteLine("ingrese numero 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese numero 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            ambosPares = num1 % 2 == 0 && num2 % 2 == 0;
+            if (ambosPares)
+                Console.WriteLine("Son pares");
+            else
+                Console.WriteLine("Son impares");
+            Console.Read();
+        }
+    }
+}
---
+++ clase/practica/ejercicio 3.5.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Crea una versión alternativa del ejercicio 3.5.3, que use "if" en vez del operador condicional
+namespace ejercicio_3._5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.WriteLine("ingrese numero 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese numero 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 % 2 == 0 && num2 % 2 == 0)
+                Console.WriteLine("son pares");
+            else
+                Console.WriteLine("son inpares");
+            Console.ReadLine();
+        }
+    }
+}
