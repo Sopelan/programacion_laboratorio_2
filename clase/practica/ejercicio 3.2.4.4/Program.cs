20e681d35d1077136463dc6dbea07c66dfd4c23d
sopelan <marcossopelana@gmail.com>
2019-10-15 12:28:03 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ clase/practica/ejercicio 3.2.4.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Crea un programa que pida números binarios al usuario y muestre su equivalente en sistema hexadecimal y en decimal. Debe repetirse hasta que el usuario introduzca la palabra "fin". */
+namespace ejercicio_3._2._4._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string binario;
+            while (true) 
+            {
+                Console.WriteLine("ingrese un numero");
+                binario = Console.ReadLine();
+                if(binario == "fin")
+                {
+                    break;
+                }
+                Console.WriteLine("el numero en decimal: " + Convert.ToInt32(binario, 2));
+                Console.WriteLine("el numero en xexadecimal: " + Convert.ToString(Convert.ToInt32(binario, 2), 16));
+
+            } 
+        }
+    }
+}
