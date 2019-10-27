23db0710175de3bd9c26265d6f1ed3ff5c61980b
sopelan <marcossopelana@gmail.com>
2019-10-27 13:24:42 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

---
+++ clase/practica/ejercicio 4.1.5.1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*  Amplía el ejemplo anterior (36b) para que avise si el dato buscado no aparece. */
+namespace ejercicio_4._1._5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dato = { 10, 15, 12, 0, 0 };
+
+            int capacidad = 5; // Capacidad maxima del array
+            int cantidad = 3; // Número real de datos guardados
+
+            int i; // Para recorrer los elementos
+
+            bool flag = false;
+            // Mostramos el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", dato[i]);
+            Console.WriteLine();
+
+            // Buscamos el dato "15"
+            for (i = 0; i < cantidad; i++)
+            {
+                if (dato[i] == 15)
+                {
+                     Console.WriteLine("15 encontrado en la posición {0} ", i + 1);
+                    flag = true;
+                }  
+            }
+            if (flag == false)
+            {
+                Console.WriteLine("no fue encontrado");
+            }
+                
+
+                
+                
+
+            // Añadimos un dato al final
+            Console.WriteLine("Añadiendo 6 al final");
+            if (cantidad < capacidad)
+            {
+                dato[cantidad] = 6;
+                cantidad++;
+            }
+
+            // Y volvemos a mostrar el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", dato[i]);
+            Console.WriteLine();
+            // Borramos el segundo dato
+            Console.WriteLine("Borrando el segundo dato");
+            int posicionBorrar = 1;
+            for (i = posicionBorrar; i < cantidad - 1; i++)
+                dato[i] = dato[i + 1];
+            cantidad--;
+
+            // Y volvemos a mostrar el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", dato[i]);
+            Console.WriteLine();
+
+            // Insertamos 30 en la tercera posición
+            if (cantidad < capacidad)
+            {
+                Console.WriteLine("Insertando 30 en la posición 3");
+                int posicionInsertar = 2;
+                for (i = cantidad; i > posicionInsertar; i--)
+                    dato[i] = dato[i - 1];
+                dato[posicionInsertar] = 30;
+                cantidad++;
+            }
+
+            // Y volvemos a mostrar el array
+            for (i = 0; i < cantidad; i++)
+                Console.Write("{0} ", dato[i]);
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
---
+++ clase/practica/ejercicio 4.1.5.2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Un programa que prepare espacio para un máximo de 10 nombres. Deberá mostrar al usuario un menú que le permita realizar las siguientes 
+operaciones:
+o Añadir un dato al final de los ya existentes.
+o Insertar un dato en una cierta posición (como ya se ha comentado, los que quedén detrás deberán desplazarse "a la derecha" para dejarle hueco;
+por ejemplo, si el array contiene "hola", "adios" y se pide insertar "bien" en la segunda posición, el array pasará a contener "hola", "bien", 
+"adios".
+o Borrar el dato que hay en una cierta posición (como se ha visto, lo que estaban detrás deberán desplazarse "a la izquierda" para que no haya
+huecos; por ejemplo, si el array contiene "hola", "bien", "adios" y se pide borrar el dato de la segunda posición, el array pasará a contener 
+"hola", "adios"
+o Mostrar los datos que contiene el array.
+o Salir del programa.*/
+namespace ejercicio_4._1._5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int cantidad = 0;
+            int espacio = 0;
+            int opcion;
+            while(true)
+            {
+                Console.WriteLine("1.añadir\n2.Borrar\n3.borrar de una cierta posicion\n4.Mostrar\5.Salir");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1:
+
+                        break;
+                }
+            }
+        }
+    }
+}
