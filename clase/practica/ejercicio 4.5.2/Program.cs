d58fca20db8282270b6e46dc6407e7cadec55521
sopelan <marcossopelana@gmail.com>
2019-12-02 13:59:52 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- clase/practica/ejercicio 4.5.2/Program.cs
+++ clase/practica/ejercicio 4.5.2/Program.cs
@@ -9,8 +9,169 @@ namespace ejercicio_4._5._2
 {
     class Program
     {
+        public struct Persona
+        {
+            public string nombre;
+            public int diaDeNacimiento;
+            public int mesDeNacimiento;
+            public int añoDeNacimiento;
+        }
         static void Main(string[] args)
         {
+            Persona[] persona = new Persona[8];
+            int num = -2;
+            for(int i = 0; i< 8;i++)
+            {
+                while(true)
+                {
+                    try
+                    {
+                        Console.WriteLine("ingrese el nombre de la persona {0}", i + 1);
+                        persona[i].nombre = Console.ReadLine();
+                        Console.WriteLine("ingrese el dia de nacimiento de la persona {0}", i + 1);
+                        persona[i].diaDeNacimiento = Convert.ToInt32(Console.ReadLine());
+                        while (persona[i].diaDeNacimiento > 31 || persona[i].diaDeNacimiento <= 0)
+                        {
+                            Console.WriteLine("error del dia");
+                            Console.WriteLine("ingrese otra vez el dia de nacimiento de la persona {0}", i + 1);
+                            persona[i].diaDeNacimiento = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.WriteLine("ingrese el mes de nacimiento de la persona {0}", i + 1);
+                        persona[i].mesDeNacimiento = Convert.ToInt32(Console.ReadLine());
+                        while(persona[i].mesDeNacimiento > 12|| persona[i].mesDeNacimiento <= 0)
+                        {
+                            Console.WriteLine("error del mes");
+                            Console.WriteLine("ingrese otra vez el mes de nacimiento de la persona {0}", i + 1);
+                            persona[i].mesDeNacimiento = Convert.ToInt32(Console.ReadLine());
+                        }
+                        Console.WriteLine("ingrese el año de nacimiento de la persona {0}", i + 1);
+                        persona[i].añoDeNacimiento = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            while(num != 0)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("ingrese el mes que buscar");
+                        num = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch(Exception Message)
+                    {
+                        Console.WriteLine(Message.Message);
+                    }
+                }
+                switch(num)
+                {
+                    case 1:
+                        Console.WriteLine("En enero cumple:");
+                        foreach(Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 1)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("En febrero cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 2)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine("En marzo cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 3)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("En abril cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 4)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 5:
+                        Console.WriteLine("En mayo cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 5)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 6:
+                        Console.WriteLine("En junio cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 6)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 7:
+                        Console.WriteLine("En julio cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 7)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 8:
+                        Console.WriteLine("En agosto cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 8)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 9:
+                        Console.WriteLine("En septiembre cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 9)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 10:
+                        Console.WriteLine("En octubre cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 10)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 11:
+                        Console.WriteLine("En noviembre cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 11)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+                    case 12:
+                        Console.WriteLine("En diciembre cumple:");
+                        foreach (Persona p in persona)
+                        {
+                            if (p.mesDeNacimiento == 12)
+                                Console.WriteLine(p.nombre + " el dia " + p.diaDeNacimiento);
+                        }
+                        break;
+
+                }
+            }
+            
+
         }
     }
 }
---
+++ clase/practica/ejercicio 4.5.3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Un programa que sea capaz de almacenar los datos de 50 personas: nombre, dirección, teléfono, edad (usando una tabla de structs). Deberá ir pidiendo los datos uno por uno, hasta que un nombre se introduzca vacío 
+ * (se pulse Intro sin teclear nada). Entonces deberá aparecer un menú que permita: 
+ * o Mostrar la lista de todos los nombres. 
+ * o Mostrar las personas de una cierta edad. 
+ * o Mostrar las personas cuya inicial sea la que el usuario indique. 
+ * o Salir del programa (lógicamente, este menú debe repetirse hasta que se escoja la opción de "salir"). */
+namespace ejercicio_4._5._3
+{
+    class Program
+    {
+        public struct Persona
+        {
+            public string nombre;
+            public string direccion;
+            public int telefono;
+            public int edad;
+        }
+        static void Main(string[] args)
+        {
+            Persona[] personas = new Persona[50];
+            int cantidad = 0;
+            int option = 0;
+            int ciertaEdad;
+            string cuyaInicial;
+            for(int i = 0;  i < 50; i++)
+            {
+                Console.WriteLine("ingrese el nombre de la persona{0}", i + 1);
+                personas[i].nombre = Console.ReadLine();
+                if(personas[i].nombre == "")
+                {
+                    break;
+                }
+                Console.WriteLine("ingrese la direccion de la persona{0}", i + 1);
+                personas[i].direccion = Console.ReadLine();
+                Console.WriteLine("ingrese el telefono de la persona{0}", i + 1);
+                personas[i].telefono = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("ingrese la edad de la persona{0}", i + 1);
+                personas[i].edad = Convert.ToInt32(Console.ReadLine());
+                cantidad++;
+            }
+            while(option != 4)
+            {
+                Console.WriteLine("1.Mostrar la lista de todos los nombres.");
+                Console.WriteLine("2.Mostrar las personas de una cierta edad.");
+                Console.WriteLine("3.Mostrar las personas cuya inicial sea la que el usuario indique.");
+                Console.WriteLine("4.Salir del programa");
+                try
+                {
+                    option = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    option = 5;
+                }
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("1.Mostrar la lista de todos los nombres.");
+                        for (int i = 0; i < cantidad; i++)
+                        {
+                            Console.WriteLine("Persona " + (i + 1));
+                            Console.WriteLine("nombre: " + personas[i].nombre);
+                            Console.WriteLine("direccion: " + personas[i].direccion);
+                            Console.WriteLine("telefono: " + personas[i].telefono);
+                            Console.WriteLine("edad: " + personas[i].edad);
+                        }
+                        break;
+                
+                }
+            }
+            
+        }
+    }
+}
