8b11e89b3bd60c7271142cd78b28488125031c6b
sopelan <marcossopelana@gmail.com>
2019-12-15 16:07:25 -0300

    Signed-off-by: sopelan <marcossopelana@gmail.com>

--- Sopelana.Marcos/Ejercicio_17.Entidades/Boligrafo.cs
+++ Sopelana.Marcos/Ejercicio_17.Entidades/Boligrafo.cs
@@ -12,7 +12,11 @@ namespace Ejercicio_17.Entidades
         private ConsoleColor color;
         private short tinta;
 
-        public Boligrafo(short tinta , ConsoleColor color) { }
+        public Boligrafo(short tinta , ConsoleColor color)
+        {
+            this.SetTinta(tinta);
+            this.color = color;
+        }
 
         public ConsoleColor GetColor()
         {
@@ -22,9 +26,39 @@ namespace Ejercicio_17.Entidades
         {
             return this.tinta;
         }
-        public bool Pintar(int gasto, out string dibujo) { }
-        public void Recargar() { }
-        public void SetTinta(short tinta);
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            if(gasto <= tinta)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < gasto; i++)
+                    sb.Append("*");
+                dibujo = sb.ToString();
+                this.tinta-= (short)gasto;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("no se pudo gastar de mas");
+                dibujo = "";
+                return false;
+            }
+        }
+        public void Recargar()
+        {
+            this.SetTinta(cantidadTintaMaxima);
+        }
+        private void SetTinta(short tinta)
+        {
+            short tint = this.tinta;
+            tint += tinta;
+            if (tint > cantidadTintaMaxima)
+                this.tinta = cantidadTintaMaxima;
+            else if (tinta < 0)
+                this.tinta = 0;
+            else
+                this.tinta = tint;      
+        }
 
     }
 }
--- Sopelana.Marcos/Ejercicio_17/Program.cs
+++ Sopelana.Marcos/Ejercicio_17/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Ejercicio_17.Entidades;
 namespace Ejercicio_17
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Boligrafo boligrafo = new Boligrafo(100, ConsoleColor.Blue);
+            Boligrafo boligrafo2 = new Boligrafo(50, ConsoleColor.Red);
+            boligrafo.Pintar(20,out string pintar);
+            Console.WriteLine(pintar);
+            boligrafo.Pintar(90, out pintar);
+            Console.WriteLine(pintar);
+            boligrafo.Recargar();
+            boligrafo.Pintar(100, out pintar);
+            Console.WriteLine(pintar);
+            boligrafo.Pintar(100, out pintar);
+            Console.WriteLine(pintar);
+            Console.Read();
+
         }
     }
 }
